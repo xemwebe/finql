4b95c327b9bafc508906d8b9266a17466cb73212
Mark Beinker <mwb@quantlink.de>
2019-11-18 19:30:48 +0100

    Initial project setup

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # finql
-A quantitative finance toolbox 
+
+Purpose of this library is to provide over time a comprehensive toolbox
+for quantitative analysis of financial assets in rust. 
+The project is licensed under Apache 2.0 or MIT license (see files LICENSE-Apache2.0 and LICENSE-MIT).
---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+//! # finql
+//!
+//! Purpose of this library is to provide over time a comprehensive toolbox
+//! for quantitative analysis of financial assets in rust. 
+//! The project is licensed under Apache 2.0 or MIT license (see files LICENSE-Apache2.0 and LICENSE-MIT).
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
