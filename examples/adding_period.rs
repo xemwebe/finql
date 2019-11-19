50a33f53318b2d3fc81a5fe337e432cbb4c2114b
Mark Beinker <mwb@quantlink.de>
2019-11-19 18:06:09 +0100

    Example extended to test with manual input.

--- examples/adding_period.rs
+++ examples/adding_period.rs
@@ -1,11 +1,18 @@
 use finql;
-use chrono::NaiveDate;
+use chrono::Utc;
 use finql::TimePeriod;
+#[macro_use] extern crate text_io;
 
 
 fn main() {
-    let date = NaiveDate::from_ymd(2019,11,18);
-    let date_in_three_months = finql::QUARTERLY.add_to(date);
-    println!("The date a quarter year after {} is the date {}.", date, date_in_three_months);
-    println!("The date has been calculated by adding the period {} to the original date.", finql::QUARTERLY)
+    let today = Utc::now().naive_local().date();
+    let date_in_three_months = finql::QUARTERLY.add_to(today);
+    println!("The date a quarter year after {} is the date {}.", today, date_in_three_months);
+    println!("The date has been calculated by adding the period {} to the original date.", finql::QUARTERLY);
+
+    println!("Try it yourself, enter an arbitrary time period:");
+    let input: String = read!("{}\n");
+    let period = finql::time_periods::from_str(&input).unwrap();
+    let future_date = period.add_to(today);
+    println!("The today plus a time period of {} is {}.", period, future_date);
 }
