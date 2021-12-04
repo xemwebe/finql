910efc7758128f5ec157912e229cc3368bc5b0d8
Mark Beinker <mwb@quantlink.de>
2021-12-05 00:29:21 +0100

    QuoteHandler build after move to deadpool_sqlite

--- finql-sqlite/src/quote_handler.rs
+++ finql-sqlite/src/quote_handler.rs
@@ -7,7 +7,7 @@ use async_trait::async_trait;
 use std::sync::Arc;
 
 use finql_data::{DataError, QuoteHandler, AssetHandler,
-    Currency, Quote, Ticker, date_time_helper::to_time};
+    Currency, Quote, Ticker};
 
 use super::{SqliteDB, SQLiteError};
 use deadpool_sqlite::rusqlite::params;
@@ -24,6 +24,8 @@ impl QuoteHandler for SqliteDB {
     // insert, get, update and delete for market data sources
     async fn insert_ticker(&self, ticker: &Ticker) -> Result<usize, DataError> {
         let ticker = ticker.to_owned();
+        let ticker_name = ticker.name.clone();
+        let ticker_source = ticker.source.clone();
         let curr = ticker.currency.to_string();
         let _ = self.conn.interact(move |conn| -> Result<(), SQLiteError> {
             conn.execute(
@@ -46,7 +48,7 @@ impl QuoteHandler for SqliteDB {
         self.conn.interact(move |conn| -> Result<usize, SQLiteError> {
             Ok(conn.query_row(
                 "SELECT id FROM ticker WHERE name=? AND source=?",
-                params![&ticker.name, &ticker.source],
+                params![&ticker_name, &ticker_source],
                 |row| row.get(0) )?)
         }).await.map_err(|e| DataError::DataAccessFailure(e.to_string()))?
         .map_err(|e| DataError::DataAccessFailure(e.to_string()))
@@ -152,7 +154,7 @@ impl QuoteHandler for SqliteDB {
         &self,
         asset_id: usize,
     ) -> Result<Vec<Ticker>, DataError> {
-        self.conn.interact(|conn| -> Result<Vec<Ticker>, SQLiteError> {
+        self.conn.interact(move |conn| -> Result<Vec<Ticker>, SQLiteError> {
             let mut stmt = conn.prepare("SELECT id, name, source, priority, currency, \
             factor, tz, cal FROM ticker WHERE asset_id=?")?;
             let ticker: Vec<Ticker> = stmt.query_map(params![&asset_id], |row| {
@@ -214,193 +216,170 @@ impl QuoteHandler for SqliteDB {
         .map_err(|e| DataError::DataAccessFailure(e.to_string()))
     }
 
-    // // insert, get, update and delete for market data sources
-    // async fn insert_quote(&self, quote: &Quote) -> Result<usize, DataError> {
-    //     let ticker_id = quote.ticker as i64;
-    //     sqlx::query!(
-    //             "INSERT INTO quotes (ticker_id, price, time, volume) 
-    //             VALUES (?, ?, ?, ?)",
-    //             ticker_id,
-    //             quote.price,
-    //             quote.time,
-    //             quote.volume,
-    //         ).execute(&self.pool).await
-    //         .map_err(|e| DataError::InsertFailed(e.to_string()))?;
-    //     let row = sqlx::query!(
-    //             "SELECT id FROM quotes WHERE ticker_id=? and time=?",
-    //             ticker_id, quote.time,
-    //         ).fetch_one(&self.pool).await
-    //         .map_err(|e| DataError::NotFound(e.to_string()))?;
-    //     Ok(row.id as usize)
-    // }
+    // insert, get, update and delete for market data sources
+    async fn insert_quote(&self, quote: &Quote) -> Result<usize, DataError> {
+        let quote = quote.to_owned();
+        let quote_ticker = quote.ticker.clone();
+        let quote_time = quote.time.clone();
+        let _ = self.conn.interact(move |conn| -> Result<(), SQLiteError> {
+            conn.execute(
+                "INSERT INTO quotes (ticker_id, price, time, volume) \
+                VALUES (?, ?, ?, ?)",
+                params![&quote.ticker, quote.price, quote.time, quote.volume])?;
+            Ok(())
+        }).await.map_err(|e| DataError::DataAccessFailure(e.to_string()))?
+        .map_err(|e| DataError::DataAccessFailure(e.to_string()));
 
-    // async fn get_last_quote_before(
-    //     &self,
-    //     asset_name: &str,
-    //     time: DateTime<Local>,
-    // ) -> Result<(Quote, Currency), DataError> {
-    //     let row = sqlx::query!(
-    //             "SELECT q.id, q.ticker_id, q.price, q.time, q.volume, t.currency, t.priority
-    //             FROM quotes q, ticker t, assets a 
-    //             WHERE a.name=? AND t.asset_id=a.id AND t.id=q.ticker_id AND q.time<=?
-    //             ORDER BY q.time DESC, t.priority ASC LIMIT 1",
-    //             asset_name, time,
-    //         ).fetch_one(&self.pool).await
-    //         .map_err(|e| DataError::NotFound(e.to_string()))?;
+        self.conn.interact(move |conn| -> Result<usize, SQLiteError> {
+            Ok(conn.query_row(
+                "SELECT id FROM quotes WHERE ticker_id=? and time=?",
+                params![&quote_ticker, &quote_time],
+                |row| row.get(0) )?)
+        }).await.map_err(|e| DataError::DataAccessFailure(e.to_string()))?
+        .map_err(|e| DataError::DataAccessFailure(e.to_string()))
+    }
 
-    //     let id = row.id;
-    //     let ticker = row.ticker_id;
-    //     let price = row.price;
-    //     let time = row.time;
-    //     let volume = row.volume;
-    //     let currency = row.currency;
-    //     let currency =
-    //         Currency::from_str(&currency).map_err(|e| DataError::NotFound(e.to_string()))?;
-    //     Ok((
-    //         Quote {
-    //             id: Some(id as usize),
-    //             ticker: ticker as usize,
-    //             price: price.into(),
-    //             time: to_time(&time, 0).map_err(|e| DataError::NotFound(e.to_string()))?,
-    //             volume: volume.map(|x| x as f64),
-    //         },
-    //         currency,
-    //     ))
-    // }
+    async fn get_last_quote_before(
+        &self,
+        asset_name: &str,
+        time: DateTime<Local>,
+    ) -> Result<(Quote, Currency), DataError> {
+        let asset = asset_name.to_owned();
+        self.conn.interact(move |conn| -> Result<(Quote, Currency), SQLiteError> {
+            Ok(conn.query_row(
+                "SELECT q.id, q.ticker_id, q.price, q.time, q.volume, t.currency, t.priority \
+                FROM quotes q, ticker t, assets a \
+                WHERE a.name=? AND t.asset_id=a.id AND t.id=q.ticker_id AND q.time<=? \
+                ORDER BY q.time DESC, t.priority ASC LIMIT 1",
+                params![&asset, &time],
+                |row| { 
+                    let currency: String = row.get(5)?;
+                    let currency = Currency::from_str(&currency)
+                        .map_err(|e| rusqlite::Error::InvalidParameterName(format!("{}", e)))?;
+                    Ok((Quote {
+                    id: row.get(0)?,
+                    ticker: row.get(1)?,
+                    price: row.get(2)?,
+                    time: row.get(3)?,
+                    volume: row.get(4)?,
+                }, currency))
+            })?)
+        }).await.map_err(|e| DataError::DataAccessFailure(e.to_string()))?
+        .map_err(|e| DataError::DataAccessFailure(e.to_string()))
+    }
 
-    // async fn get_last_quote_before_by_id(
-    //     &self,
-    //     asset_id: usize,
-    //     time: DateTime<Local>,
-    // ) -> Result<(Quote, Currency), DataError> {
-    //     let asset_id = asset_id as i32;
-    //     let row = sqlx::query!(
-    //             "SELECT q.id, q.ticker_id, q.price, q.time, q.volume, t.currency, t.priority
-    //             FROM quotes q, ticker t
-    //             WHERE t.asset_id=?1 AND t.id=q.ticker_id AND q.time<= ?2
-    //             ORDER BY q.time DESC, t.priority ASC LIMIT 1",
-    //             asset_id, time,
-    //         ).fetch_one(&self.pool).await
-    //         .map_err(|e| DataError::NotFound(e.to_string()))?;
+    async fn get_last_quote_before_by_id(
+        &self,
+        asset_id: usize,
+        time: DateTime<Local>,
+    ) -> Result<(Quote, Currency), DataError> {
+        self.conn.interact(move |conn| -> Result<(Quote, Currency), SQLiteError> {
+            Ok(conn.query_row(
+                "SELECT q.id, q.ticker_id, q.price, q.time, q.volume, t.currency, t.priority \
+                FROM quotes q, ticker t \
+                WHERE t.asset_id=?1 AND t.id=q.ticker_id AND q.time<= ?2 \
+                ORDER BY q.time DESC, t.priority ASC LIMIT 1",
+                params![&asset_id, &time],
+                |row| { 
+                    let currency: String = row.get(5)?;
+                    let currency = Currency::from_str(&currency)
+                        .map_err(|e| rusqlite::Error::InvalidParameterName(format!("{}", e)))?;
+                    Ok((Quote {
+                    id: row.get(0)?,
+                    ticker: row.get(1)?,
+                    price: row.get(2)?,
+                    time: row.get(3)?,
+                    volume: row.get(4)?,
+                }, currency))
+            })?)
+        }).await.map_err(|e| DataError::DataAccessFailure(e.to_string()))?
+        .map_err(|e| DataError::DataAccessFailure(e.to_string()))
+    }
 
-    //     let id = row.id;
-    //     let ticker = row.ticker_id;
-    //     let price = row.price;
-    //     let time = row.time;
-    //     let volume = row.volume;
-    //     let currency = row.currency;
-    //     let currency =
-    //         Currency::from_str(&currency).map_err(|e| DataError::NotFound(e.to_string()))?;
-    //     Ok((
-    //         Quote {
-    //             id: Some(id as usize),
-    //             ticker: ticker as usize,
-    //             price: price.into(),
-    //             time: to_time(&time, 0).map_err(|e| DataError::NotFound(e.to_string()))?,
-    //             volume: volume.map(|x| x as f64),
-    //         },
-    //         currency,
-    //     ))
-    // }
+    async fn get_all_quotes_for_ticker(&self, ticker_id: usize) -> Result<Vec<Quote>, DataError> {
+        self.conn.interact(move |conn| -> Result<Vec<Quote>, SQLiteError> {
+            let mut stmt = conn.prepare("SELECT id, price, time, volume FROM quotes \
+            WHERE ticker_id=?1 ORDER BY time ASC")?;
+            let quotes: Vec<Quote> = stmt.query_map([&ticker_id], |row| {
+                Ok(Quote {
+                    id: row.get(0)?,
+                    ticker: ticker_id,
+                    price: row.get(1)?,
+                    time: row.get(2)?,
+                    volume: row.get(3)?,
+                })
+            })?.filter_map(|quote| quote.ok() ).collect();
+            Ok(quotes)
+        })
+        .await.map_err(|e| DataError::DataAccessFailure(e.to_string()))?
+        .map_err(|e| DataError::DataAccessFailure(e.to_string()))
+    }
 
-    // async fn get_all_quotes_for_ticker(&self, ticker_id: usize) -> Result<Vec<Quote>, DataError> {
-    //     let mut quotes = Vec::new();
-    //     let t_id = ticker_id as i32;
-    //     for row in sqlx::query!(
-    //             "SELECT id, price, time, volume FROM quotes 
-    //             WHERE ticker_id=?1 ORDER BY time ASC",
-    //             t_id,
-    //         ).fetch_all(&self.pool).await
-    //         .map_err(|e| DataError::NotFound(e.to_string()))?
-    //     {
-    //         let id = row.id;
-    //         let time = row.time;
-    //         quotes.push(Quote {
-    //             id: Some(id as usize),
-    //             ticker: ticker_id,
-    //             price: row.price.into(),
-    //             time: to_time(&time, 0).map_err(|e| DataError::NotFound(e.to_string()))?,
-    //             volume: row.volume.map(|x| x as f64),
-    //         });
-    //     }
-    //     Ok(quotes)
-    // }
+    async fn update_quote(&self, quote: &Quote) -> Result<(), DataError> {
+        if let Some(id) = quote.id {
+            let quote = quote.to_owned();
+            self.conn.interact(move |conn| -> Result<(), SQLiteError> {
+                conn.execute(
+                    "UPDATE quotes SET ticker_id=?2, price=?3, time=?4, volume=?5 \
+                    WHERE id=?1",
+                    params![&id, &quote.ticker, &quote.price, &quote.time, &quote.volume])?;
+                Ok(())
+            }).await.map_err(|e| DataError::DataAccessFailure(e.to_string()))?
+            .map_err(|e| DataError::DataAccessFailure(e.to_string()))
+        } else {
+            Err(DataError::NotFound(
+                "not yet stored to database".to_string(),
+            ))
+        }
+    }
 
-    // async fn update_quote(&self, quote: &Quote) -> Result<(), DataError> {
-    //     if quote.id.is_none() {
-    //         return Err(DataError::NotFound(
-    //             "not yet stored to database".to_string(),
-    //         ));
-    //     }
-    //     let id = quote.id.unwrap() as i32;
-    //     let ticker_id = quote.ticker as i32;
-    //     sqlx::query!(
-    //             "UPDATE quotes SET ticker_id=?2, price=?3, time=?4, volume=?5
-    //             WHERE id=?1",
-    //             id,
-    //             ticker_id,
-    //             quote.price,
-    //             quote.time,
-    //             quote.volume,
-    //         )
-    //         .execute(&self.pool).await
-    //         .map_err(|e| DataError::InsertFailed(e.to_string()))?;
-    //     Ok(())
-    // }
+    async fn delete_quote(&self, id: usize) -> Result<(), DataError> {
+        self.conn.interact(move |conn| -> Result<(), SQLiteError> {
+            conn.execute("DELETE FROM quotes WHERE id=?1", params![&id])?;
+            Ok(())
+        }).await.map_err(|e| DataError::DataAccessFailure(e.to_string()))?
+        .map_err(|e| DataError::DataAccessFailure(e.to_string()))
+    }
 
-    // async fn delete_quote(&self, id: usize) -> Result<(), DataError> {
-    //     let id = id as i64;
-    //     sqlx::query!("DELETE FROM quotes WHERE id=?1;", id)
-    //         .execute(&self.pool).await
-    //         .map_err(|e| DataError::InsertFailed(e.to_string()))?;
-    //     Ok(())
-    // }
+    async fn remove_duplicates(&self) -> Result<(), DataError> {
+        self.conn.interact(|conn| -> Result<(), SQLiteError> {
+            let _ = conn.execute("DELETE FROM quotes \
+            WHERE id IN \
+            (SELECT q2.id \
+            FROM \
+                quotes q1, \
+                quotes q2 \
+            WHERE \
+                q1.id < q2.id \
+            AND q1.ticker_id = q2.ticker_id \
+            AND q1.time = q2.time \
+            AND q1.price = q2.price)", []);
+            Ok(())
+        }).await.map_err(|e| DataError::DataAccessFailure(e.to_string()))?
+        .map_err(|e| DataError::DataAccessFailure(e.to_string()))
+    }
 
+    async fn get_rounding_digits(&self, currency: Currency) -> i32 {
+        let curr = currency.to_string();
+        self.conn.interact(move |conn| -> Result<i32, SQLiteError> {
+            Ok(conn.query_row(
+                "SELECT digits FROM rounding_digits WHERE currency=?1",
+                params![&curr],
+                |row| { 
+                    let digit: i32 = row.get(0)?;
+                    Ok(digit)
+                })?)
+        }).await.unwrap_or(Ok(2)).unwrap_or(2)
+    }
 
-    // async fn remove_duplicates(&self) -> Result<(), DataError> {
-    //     sqlx::query!("
-    //         delete from quotes 
-    //         where id in
-    //         (select q2.id
-    //         from 
-    //             quotes q1,
-    //             quotes q2
-    //         where 
-    //             q1.id < q2.id
-    //         and q1.ticker_id = q2.ticker_id 
-    //         and q1.time = q2.time
-    //         and q1.price = q2.price) 
-    //         ")
-    //         .execute(&self.pool).await
-    //         .map_err(|e| DataError::DeleteFailed(e.to_string()))?;
-    //     Ok(())
-    // }
-
-    // async fn get_rounding_digits(&self, currency: Currency) -> i32 {
-    //     let curr = currency.to_string();
-    //     let rows = sqlx::query!(
-    //         "SELECT digits FROM rounding_digits WHERE currency=?1;",
-    //         curr,
-    //     ).fetch_all(&self.pool).await;
-    //     match rows {
-    //         Ok(row_vec) => {
-    //             if !row_vec.is_empty() {
-    //                 row_vec[0].digits as i32
-    //             } else {
-    //                 2
-    //             }
-    //         }
-    //         Err(_) => 2,
-    //     }
-    // }
-
-    // async fn set_rounding_digits(&self, currency: Currency, digits: i32) -> Result<(), DataError> {
-    //     let curr = currency.to_string();
-    //     let _row = sqlx::query!(
-    //             "INSERT INTO rounding_digits (currency, digits) VALUES (?1, ?2)",
-    //             curr, digits,
-    //         ).execute(&self.pool).await
-    //         .map_err(|e| DataError::InsertFailed(e.to_string()))?;
-    //     Ok(())
-    // }
+    async fn set_rounding_digits(&self, currency: Currency, digits: i32) -> Result<(), DataError> {
+        let curr = currency.to_string();
+        self.conn.interact(move |conn| -> Result<(), SQLiteError> {
+            let _ = conn.execute(
+                "INSERT INTO rounding_digits (currency, digits) VALUES (?1, ?2)",
+                params![&curr, &digits]);
+            Ok(())
+        }).await.map_err(|e| DataError::DataAccessFailure(e.to_string()))?
+        .map_err(|e| DataError::DataAccessFailure(e.to_string()))
+    }
 }
